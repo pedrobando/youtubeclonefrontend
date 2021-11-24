3f20b90e78fc4b6304272e4e763352adf2924b96
Pedro Rivera <pedrorivera@Pedros-Mac-mini.local>
2021-11-24 13:24:59 -0600

    Add youtube component to get all the videos and display  related videos.

--- src/components/RelatedVideos/RelatedVideos.jsx
+++ src/components/RelatedVideos/RelatedVideos.jsx
@@ -1,17 +1,36 @@
 import React from "react";
-import { Row, Col} from "react-bootstrap/";
+import { Row, Col } from "react-bootstrap/";
+import "./RelatedVideos.css";
 
+const RelatedVideos = (props) => {
+  const videos = props.videos;
+  const videosListItems = videos.map(
+    (videos, index) =>
+      index < 3 && (
+        <div className="related_videos_list_item">
+          <div className="videoThumnail">
+            <a href="">
+              <img
+                src={videos.snippet.thumbnails.default.url}
+                alt={videos.snippet.title}
+              />
+            </a>
+          </div>
+          <h3>{videos.snippet.title}</h3>
+          <span>{videos.snippet.channelTitle}</span>
+          <br className="clear" />
+        </div>
+      )
+  );
+  console.log(videosListItems);
+  return (
+    <Row className="related_videos">
+      <Col xs={6} md={6}>
+        <h3> React Related Videos</h3>
+        {videosListItems}
+      </Col>
+    </Row>
+  );
+};
 
-const RelatedVideos = (videos) => {
-    const videosList = videos.map();
-    return ( 
-        <Row className="related_videos">
-            <Col xs={6} md={6}>
-                <h3>Related Videos</h3>
-
-            </Col>
-        </Row>
-     );
-}
- 
 export default RelatedVideos;
